334fa442dbbd891037ecac13579d618391cc4000
sorya.loli <47668985+sssorya@users.noreply.github.com>
2023-07-29 17:58:20 +0300

    Update view-posts.php

--- view-posts.php
+++ view-posts.php
@@ -1,1 +1,6 @@
-
+<?php
+  header("Content-type: text/html; charset=uts-8");?>
+<meta http-equiv="Content-Type" content=text/html; charset=utf-8"/>
+<?php require_once('includes/temps/header.php');?>
+<br/>
+<a href="<?php echo $this->base->url; ?>" class="btn-primary">Вернуться к списку сообщений </a>
