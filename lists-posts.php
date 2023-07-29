83b46df297ac66392f0daa276e2c998a2f3571fe
sorya.loli <47668985+sssorya@users.noreply.github.com>
2023-07-29 17:53:22 +0300

    Create lists-posts.php

---
+++ lists-posts.php
@@ -0,0 +1,9 @@
+<?php
+  header("Content-type: text/html; charset=uts-8");?>
+<meta http-equiv="Content-Type" content=text/html; charset=utf-8"/>
+<?php require_once '/includes/temps/header.php';?>
+<php foreach ($posts as $post) :?>
+<h3> Сообщение №<?php echo $post['id'];?></h3>
+<p><?php echo implode('', array_slice(explode('', 
+    strip_tags($post['content'], 0, 10)); ?></p>
+<a href="<?php echo $this->base->url."/?id=".$post['id']; ?>" class="btn-primary">Подробнее</a>
