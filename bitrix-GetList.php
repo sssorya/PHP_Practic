bc14f8f4eaf22ed34e2eb009f3abc823aef5302c
sorya <47668985+sssorya@users.noreply.github.com>
2023-08-08 22:34:50 +0300

    Update bitrix-GetList.php

--- bitrix-GetList.php
+++ bitrix-GetList.php
@@ -1,7 +1,6 @@
-////////////////////////////////////////////////////
-// получение списка активных товаров без изображений
-////////////////////////////////////////////////////
-
+////////////////////////
+// товары без детальной 
+///////////////////////
 $IBLOCK_ID = 33;
 $col = 0;
 $filt = array("IBLOCK_ID"=>$IBLOCK_ID, "DETAIL_PICTURE" => false, "ACTIVE" => "Y");
@@ -14,8 +13,29 @@ while($tov = $res->fetch())
 	{
 		echo($proper["VALUE"]."<pre>");
 	}
+	//$col++;
+	echo '<br>Порядковый номер - '. $tov['ID'];
+	echo '<pre>';
+    print_r($tov);
+	echo '</pre>';
+}
+/////////////////////////////////
+// товары без дополнительных фото
+/////////////////////////////////
+$IBLOCK_ID = 33;
+$col = 0;
+$filt = array("IBLOCK_ID"=>$IBLOCK_ID, "MORE_PHOTO" => false, "ACTIVE" => "Y");
+$res = CIBlockElement::GetList(array("ID"=>"ASC"), $filt, false, false, array("NAME"));
+$props = CIBlockElement::GetProperty("IBLOCK_ID", $tov["ID"],array("sort"=>"ASC"));
+
+while($tov = $res->fetch())
+{
+	if($proper = $props->fetch()) 
+	{
+		echo($proper["VALUE"]."<pre>");
+	}
 	$col++;
-	echo '<br>Порядковый номер - '. $col;
+	echo '<br>Порядковый номер - '. $col;//$tov['ID'];
 	echo '<pre>';
     print_r($tov);
 	echo '</pre>';
