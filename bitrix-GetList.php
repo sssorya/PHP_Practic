08b8a6aae5ab9c8d30e221d4c3a032425b0c678b
sorya <47668985+sssorya@users.noreply.github.com>
2023-08-11 18:26:19 +0300

    Update bitrix-GetList.php

--- bitrix-GetList.php
+++ bitrix-GetList.php
@@ -52,3 +52,52 @@ while($info = $prod->fetch()){
 	print_r($info);
 	echo '</pre>';
 }
+//////////////////////////
+// Сортировка товара по ID
+//////////////////////////
+$prod = CCatalogProduct::Getlist ([
+		'filter' => ['ID' => 'ASC']
+]);
+
+while($info = $prod->fetch()){
+	echo '<pre>';
+	print_r($info);
+	echo '</pre>';
+}
+/////////////////
+$IBLOCK_ID = 33;
+
+$product = CIBlockElement::GetList(
+		array('SORT'=>'ASC')
+	);
+
+while($pok = $product->fetch()){
+	echo '<pre>';
+	print_r($pok);
+	echo '</pre>';
+}
+/////////////////////////
+// все товары и свойства
+/////////////////////////
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+use \Bitrix\Main\Loader;
+use \Bitrix\Main\Loader::IncludeModule("catalog");
+use \Bitrix\Main\Loader::IncludeModule('iblock');
+
+$IBLOCK_ID = 33;
+
+$arSelect = Array("ID", "IBLOCK_ID", "NAME", "DATE_ACTIVE_FROM",);
+$arFilter = Array("IBLOCK_ID"=>33);
+$res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>500), $arSelect);
+
+while($ob = $res->GetNextElement()){
+	$arFields = $ob->GetFields();
+		echo '<pre>';
+		print_r($arFields);
+		echo '</pre>';
+        $arProps = $ob->GetProperties();
+		echo '<pre>';
+		print_r($arProps);
+		echo '</pre>';
+}
